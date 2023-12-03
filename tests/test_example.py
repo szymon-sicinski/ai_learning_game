3eff6be90b0636adface439e8addaa0c2661465b
szymon-sicinski <szym.sicinski@gmail.com>
2023-12-03 21:14:08 +0100

    Delete sample tests

--- tests/test_example.py
+++
@@ -1,8 +0,0 @@
-# simple test class  to test pytest
-class TestExample:
-    def test_always_passes(self):
-        assert 1 + 1 == 2
-
-    # sample test that will fail
-    def test_always_fails(self):
-        assert 1 + 2 == 3
