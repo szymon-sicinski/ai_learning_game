62a587e9ab74533e93cce3ead6f104b7da4c0c84
szymon-sicinski <szym.sicinski@gmail.com>
2023-12-03 15:01:20 +0100

    Sample tests for template

--- main-TEST.py
+++ main-TEST.py
@@ -1,30 +1,29 @@
-# Simple pygame program
-
-# Import and initialize the pygame library
 import pygame
 
-pygame.init()
 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+def start_game():
+    pygame.init()
 
-# Run until the user asks to quit
-running = True
-while running:
+    # Set up the drawing window
+    screen = pygame.display.set_mode([500, 500])
 
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    # Run until the user asks to quit
+    running = True
+    while running:
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+        # Fill the background with white
+        screen.fill((255, 255, 255))
 
-    # Flip the display
-    pygame.display.flip()
+        # Draw a solid blue circle in the center
+        pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
 
-# Done! Time to quit.
-pygame.quit()
+        # Flip the display
+        pygame.display.flip()
+
+    # Done! Time to quit.
+    pygame.quit()
