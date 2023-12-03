3538855e86929a7f792727297e2116cc9e259da0
szymon-sicinski <szym.sicinski@gmail.com>
2023-12-03 14:36:07 +0100

    Change to test class

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,8 @@
-def test_always_passes():
-    assert 1 + 1 == 2
+# simple test class  to test pytest
+class TestExample:
+    def test_always_passes(self):
+        assert 1 + 1 == 2
 
-# sample test that will fail
-def test_always_fails():
-    assert 1 + 2 == 3
-    
+    # sample test that will fail
+    def test_always_fails(self):
+        assert 1 + 2 == 3
